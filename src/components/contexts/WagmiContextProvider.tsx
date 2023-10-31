7bd4d4b9711a81a6782c57e35e48d28a41576195
Galih Laras Prakoso <galihlprakoso@gmail.com>
2023-11-01 02:27:06 +0700

    -

--- src/components/contexts/WagmiContextProvider.tsx
+++ src/components/contexts/WagmiContextProvider.tsx
@@ -3,13 +3,6 @@ import { ReactNode } from 'react'
 import { WagmiConfig } from 'wagmi'
 import { arbitrum, mainnet } from 'wagmi/chains'
 
-/**
- * TODO:
- * - for Internet Computer. Need to create custom connector.
- *  - https://github.com/Connect2IC/connect2ic/blob/main/packages/core/src/providers/internet-identity.ts
- *  - Or bisa langsung pake ini: https://github.com/Connect2IC/connect2ic tapi dipisahin buttonnya.
- */
-
 const projectId = process.env.REACT_APP_WALLET_CONNECT_PROJECT_ID as string
 
 const metadata = {
@@ -25,26 +18,15 @@ const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata })
 createWeb3Modal({
   wagmiConfig, projectId, chains,
   featuredWalletIds: [
-    'c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96',
-    'fd20dc426fb37566d803205b19bbc1d4096b248ac04548e3cfb6b3a38bd033aa',    
+    'c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96', //metamask
+    'fd20dc426fb37566d803205b19bbc1d4096b248ac04548e3cfb6b3a38bd033aa', //coinbase wallet
+    'c03dfee351b6fcc421b4494ea33b9d4b92a984f87aa76d1663bb28705e95034a', //uniswap wallet
   ],
   includeWalletIds: [
-    'c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96',
-    'fd20dc426fb37566d803205b19bbc1d4096b248ac04548e3cfb6b3a38bd033aa',    
+    'c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96', //metamask
+    'fd20dc426fb37566d803205b19bbc1d4096b248ac04548e3cfb6b3a38bd033aa', //coinbase wallet
+    'c03dfee351b6fcc421b4494ea33b9d4b92a984f87aa76d1663bb28705e95034a', //uniswap wallet
   ],
-  customWallets: [
-    {
-      id: 'myCustomWallet',
-      name: 'My Custom Wallet',
-      homepage: 'www.mycustomwallet.com', // Optional
-      image_url: 'my_custom_wallet_image', // Optional
-      mobile_link: 'mobile_link', // Optional - Deeplink or universal
-      desktop_link: 'desktop_link', // Optional - Deeplink
-      webapp_link: 'webapp_link', // Optional
-      app_store: 'app_store', // Optional
-      play_store: 'play_store' // Optional
-    }
-  ]
 })
 
 type Props = {
